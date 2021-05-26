33314e3d99705eea3185179ad00f6ca34590796b
VladioEroonda <vladioaf8@gmail.com>
2021-05-26 20:44:45 +0300

    Initial commit

---
+++ src/main/java/ru/eroonda/ticketmachine/TicketMachineApplication.java
@@ -0,0 +1,13 @@
+package ru.eroonda.ticketmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/eroonda/ticketmachine/TicketMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eroonda.ticketmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
